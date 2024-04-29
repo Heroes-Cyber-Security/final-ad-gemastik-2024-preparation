2c4fedf2205d1666aa78c3e0f5cd36bd822216de
Ignazio Daniele Di Blasi <127403997+DnyyGzd@users.noreply.github.com>
2024-04-29 15:05:13 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,3 +10,8 @@ git clone https://github.com/DnyyGzd/ForcAD
 
 pip3 install -r ForcAD/cli/requirements.txt
 chmod +x ForcAD/control.py
+
+chmod +x ForcAD/checkers/flags_shop/checker.py
+chmod +x ForcAD/checkers/cyberuni-examnotes/checker.py
+chmod +x ForcAD/checkers/cyberuni-encryptednotes/checker.py
+chmod +x ForcAD/checkers/cyberuni-examportal/checker.py
