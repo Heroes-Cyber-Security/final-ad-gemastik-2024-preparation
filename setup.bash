993afa7f6df4c329ead820b1f3f2a1e72b34af78
Ignazio Daniele Di Blasi <127403997+DnyyGzd@users.noreply.github.com>
2024-05-01 12:42:19 +0200

    Add files via upload

--- setup.bash
+++ setup.bash
@@ -5,9 +5,6 @@ sudo apt update && sudo apt full-upgrade -y
 sudo apt install -y curl git python3-pip
 sudo apt install -y docker.io docker-compose-v2
 
-sudo usermod -aG docker $USER
-newgrp docker
-
 git clone https://github.com/DnyyGzd/ForcAD
 
 pip3 install -r ForcAD/cli/requirements.txt
@@ -17,3 +14,6 @@ chmod +x ForcAD/checkers/flags_shop/checker.py
 chmod +x ForcAD/checkers/cyberuni-examnotes/checker.py
 chmod +x ForcAD/checkers/cyberuni-encryptednotes/checker.py
 chmod +x ForcAD/checkers/cyberuni-examportal/checker.py
+
+sudo usermod -aG docker $USER
+newgrp docker
